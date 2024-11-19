b92a1b3aac67ec87e5a29dd25172c3e52be981b1
Khin Thandar Tun <106916866+KhinThandarTun@users.noreply.github.com>
2024-11-19 15:05:17 +0630

    Update ProductController.cs

--- POS.API/Controllers/ProductController.cs
+++ POS.API/Controllers/ProductController.cs
@@ -18,7 +18,6 @@ namespace POS.API.Controllers
         }
 
         [HttpGet("by/{id}")]
-        [ResponseCache(Duration = 120, Location = ResponseCacheLocation.Any)]
         public IActionResult GetProductById(string id)
         {
             var products = _context.Products.FirstOrDefault(x => x.ProductId == id);
